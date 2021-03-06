2f811f04d907123a5be4492bcf0e75f00b0082cf
Gustavo Soares <gusnsoares@gmail.com>
2021-03-06 18:06:37 -0300

    Fix prospection urls

--- prospection/urls.py
+++ prospection/urls.py
@@ -5,44 +5,40 @@
 from django.urls import path
 
 # Project
-from core.views import (CloseCompany,
-                        EditCompany,
-                        EditProspector,
-                        NewCompany,
-                        NewProspector,
-                        SelectCompany,
-                        SelectProspector,
-                        success)
+from prospection.views.company_close import CompanyClose
+from prospection.views.company_edit import CompanyEdit
+from prospection.views.company_new import CompanyNew
+from prospection.views.company_select import CompanySelect
+from prospection.views.prospector_edit import ProspectorEdit
+from prospection.views.prospector_new import ProspectorNew
+from prospection.views.prospector_select import ProspectorSelect
+from prospection.views.success import success
 
 
 #
 # CODE
 #
-app_name = 'core'
+app_name = 'prospection'
 urlpatterns = [
-    # close
-    path('close_company/', CloseCompany.as_view(), name='close_company'),
+    # companies
+    path('companies/close/', CompanyClose.as_view(), name='company_close'),
+    path('companies/edit/<int:id>/',
+         CompanyEdit.as_view(),
+         name='company_edit'),
+    path('companies/new/', CompanyNew.as_view(), name='company_new'),
+    path('companies', CompanySelect.as_view(), name='company_select'),
 
-    # edit
-    path('edit_company/<int:id>/',
-         EditCompany.as_view(),
-         name='edit_company'),
-    path('edit_prospector/<int:id>/',
-         EditProspector.as_view(),
-         name='edit_prospector'),
-
-    # new
-    path('new_company/', NewCompany.as_view(), name='new_company'),
-    path('new_prospector/', NewProspector.as_view(), name='new_prospector'),
-
-    # select
-    path('select_company/', SelectCompany.as_view(), name='select_company'),
-    path('select_prospector/',
-         SelectProspector.as_view(),
-         name='select_prospector'),
+    # prospectors
+    path('prospectors/edit/<int:id>/',
+         ProspectorEdit.as_view(),
+         name='prospector_edit'),
+    path('prospectors/new/', ProspectorNew.as_view(), name='prospector_new'),
+    path('prospectors',
+         ProspectorSelect.as_view(),
+         name='prospector_select'),
 
     # success
-    path('<str:action>_<str:object>/<int:id>/success/',
+    path('<str:object>/<str:action>/<int:id>/success/',
          success,
          name='success'),
 ]
