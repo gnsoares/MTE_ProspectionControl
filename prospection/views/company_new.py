f6d7fdef3a7b70207f2d9176d453b8f34ff220fa
Gustavo Soares <gusnsoares@gmail.com>
2021-03-06 22:49:04 -0300

    Create the view of creation of companies

--- prospection/views/company_new.py
+++ prospection/views/company_new.py
@@ -1,9 +1,21 @@
 #
 # IMPORTS
 #
+# Python std library
+import os
+
 # Django
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+
+
+# Project
+from captacao_talento.views.common_context import COMMON_CONTEXT
+from prospection.forms.company_new import CompanyNew as CompanyNewForm
+from prospection.models import Company
+from trello_helper import post_card
+from utils import get_store
 
 
 #
@@ -11,8 +23,58 @@ from django.views import View
 #
 class CompanyNew(View):
 
+    form_class = CompanyNewForm
+    template_name = 'company_new.html'
+    title = 'Inclusão de Empresa'
+
     def get(self, request, *args, **kwargs):
-        return render(request, '', {})
+
+        # render page
+        return render(
+            request,
+            self.template_name,
+            {
+                **COMMON_CONTEXT,
+                'action': request.path,
+                'page_name': self.title,
+                'form': self.form_class(),
+            },
+        )
 
     def post(self, request, *args, **kwargs):
-        return render(request, '', {})
+
+        # get form data
+        form = self.form_class(request.POST)
+
+        # form is valid: create company
+        if form.is_valid():
+
+            # create company object
+            company = Company(
+                name=form.cleaned_data['name'],
+                category=form.cleaned_data['category'],
+                prospector=form.cleaned_data['seller'],
+                main_contact=form.cleaned_data['main_contact']
+            )
+
+            # create company card and save its id
+            response = post_card(
+                company.name,
+                get_store()['boards']['sales']['id']
+            )
+            company_card = response.json()
+            company.card_id = company_card['id']
+
+            # save company to db
+            company.save()
+
+            # render success page
+            return HttpResponseRedirect(
+                f'{request.path}{company.id}/success/'
+            )
+
+        else:
+            pass
+        # not debugging: return generic error message
+        if not os.environ['DEBUG']:
+            return HttpResponse('Something went wrong')
