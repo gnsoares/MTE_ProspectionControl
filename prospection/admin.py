df96f5b3c32a8ef7810877a351124506ecb9f0f2
Gustavo Soares <gusnsoares@gmail.com>
2021-03-08 23:10:55 -0300

    create contract model

--- prospection/admin.py
+++ prospection/admin.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 # Project
 from .models import Activity
 from .models import Company
+from .models import Contract
 from .models import Prospector
 
 
@@ -15,4 +16,5 @@ from .models import Prospector
 #
 admin.site.register(Activity)
 admin.site.register(Company)
+admin.site.register(Contract)
 admin.site.register(Prospector)
