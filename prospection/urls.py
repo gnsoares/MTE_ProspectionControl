6f2747f0a1161117b1696e27220a6bf0cca91b57
Gustavo Soares <gusnsoares@gmail.com>
2021-03-29 18:31:39 -0300

    enable visualization of all prospectors

--- prospection/urls.py
+++ prospection/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 # Project
 from .views.prospector_edit import ProspectorEdit
 from .views.prospector_new import ProspectorNew
+from .views.prospector_select import ProspectorSelect
 
 
 #
@@ -18,4 +19,7 @@ urlpatterns = [
          ProspectorEdit.as_view(),
          name='prospector_edit'),
     path('prospectors/new/', ProspectorNew.as_view(), name='prospector_new'),
+    path('prospectors',
+         ProspectorSelect.as_view(),
+         name='prospector_select'),
 ]
